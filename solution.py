8946763bead4e3bf45578287f39a9571452542d7
Ericg411 <ericg411@gmail.com>
2025-02-11 17:01:14 -0600

    made algorithm more efficient

--- solution.py
+++ solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        binary_string = bin(n) [2:]
         counter = 0
-        for c in binary_string:
-            if c == '1':
-                counter += 1
+        while n:
+            counter += n & 1
+            n >>= 1
         return counter
